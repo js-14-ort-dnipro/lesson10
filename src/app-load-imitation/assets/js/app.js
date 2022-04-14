b3455438117d535e104deb78cec12d5b44847a0c
Anatoliy Kigel <anatoliy.kigel@gmail.com>
2022-04-14 11:56:20 +0300

    Update

--- src/app-load-imitation/assets/js/app.js
+++ src/app-load-imitation/assets/js/app.js
@@ -1,2 +1,6 @@
+    
+    const countriesURL  = 'https://restcountries.com/v3.1/all';
 
-    const countriesURL = 'https://restcountries.com/v3.1/all';
+    const ratesURL      = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
+
+    const usersURL      = 'https://randomuser.me/api/?results=30';
